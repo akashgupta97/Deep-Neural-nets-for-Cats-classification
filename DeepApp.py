01b8b3037540b69d5dcb576cf89da02af7246450
Akash <akash.darknight@gmail.com>
2018-02-10 10:03:00 +0530

    Application file

---
+++ DeepApp.py
@@ -0,0 +1,53 @@
+
+# # Deep Neural Network for Image Classification: Application
+#
+
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn_app_utils_v2 import *
+
+get_ipython().magic('matplotlib inline')
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+get_ipython().magic('load_ext autoreload')
+get_ipython().magic('autoreload 2')
+
+np.random.seed(1)
+
+# ## 2 - Dataset
+#
+
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+# The following code will show you an image in the dataset. Feel free to change the index and re-run the cell multiple times to see other images.
+
+# In[6]:
+
+# Example of a picture
+index = 15
+plt.imshow(train_x_orig[index])
+print(train_x_orig.shape)
+print("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.")
+
+# In[7]:
+
+# Explore your dataset
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print("Number of training examples: " + str(m_train))
+print("Number of testing examples: " + str(m_test))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_x_orig shape: " + str(train_x_orig.shape))
+print("train_y shape: " + str(train_y.shape))
+print("test_x_orig shape: " + str(test_x_orig.shape))
+print("test_y shape: " + str(test_y.shape))
