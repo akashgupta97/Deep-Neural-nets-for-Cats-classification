b502dab6faba597f4e5acbc7526474a28e6d02ab
Akash <akash.darknight@gmail.com>
2018-02-12 10:03:00 +0530

    Application file

--- DeepApp.py
+++ DeepApp.py
@@ -13,3 +13,11 @@ get_ipython().magic('matplotlib inline')
 plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
+
+
+get_ipython().magic('load_ext autoreload')
+get_ipython().magic('autoreload 2')
+
+np.random.seed(1)
+
+# ## 2 - Dataset
