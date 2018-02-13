9cb12257c74d253860af6ba7a5306bec18584320
Akash <akash.darknight@gmail.com>
2018-02-13 10:03:00 +0530

    Application file

--- DeepApp.py
+++ DeepApp.py
@@ -21,3 +21,15 @@ get_ipython().magic('autoreload 2')
 np.random.seed(1)
 
 # ## 2 - Dataset
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+# The following code will show you an image in the dataset. Feel free to change the index and re-run the cell multiple times to see other images.
+
+# In[6]:
+
+# Example of a picture
+index = 15
+plt.imshow(train_x_orig[index])
+print(train_x_orig.shape)
+print("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.")
