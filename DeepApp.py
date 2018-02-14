351b157da90af4d641a25491a9417924c8a7bb51
Akash <akash.darknight@gmail.com>
2018-02-14 10:03:00 +0530

    Application file

--- DeepApp.py
+++ DeepApp.py
@@ -33,3 +33,18 @@ index = 15
 plt.imshow(train_x_orig[index])
 print(train_x_orig.shape)
 print("y = " + str(train_y[0, index]) + ". It's a " + classes[train_y[0, index]].decode("utf-8") + " picture.")
+
+
+
+# Explore your dataset
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print("Number of training examples: " + str(m_train))
+print("Number of testing examples: " + str(m_test))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_x_orig shape: " + str(train_x_orig.shape))
+print("train_y shape: " + str(train_y.shape))
+print("test_x_orig shape: " + str(test_x_orig.shape))
+print("test_y shape: " + str(test_y.shape))
