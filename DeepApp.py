adde6ab52ca04fc5e75d2d7f5ebcfb9d6af78c99
Akash <akash.darknight@gmail.com>
2018-03-05 10:03:00 +0530

    Application file

--- DeepApp.py
+++ DeepApp.py
@@ -397,3 +397,12 @@ print_mislabeled_images(classes, test_x, test_y, pred_test)
 # - Camera Angle
 # - Brightness of the picture
 # - Scale variation (cat is very large or small in image)
+## START CODE HERE ##
+my_image = "my_image.jpg"  # change this to the name of your image file
+my_label_y = [1]  # the true class of your image (1 -> cat, 0 -> non-cat)
+## END CODE HERE ##
+
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px, num_px)).reshape((num_px * num_px * 3, 1))
+my_predicted_image = predict(my_image, my_label_y, parameters)
