52401a7b65bdc83f602a7183f30f399e77c1386f
Akash <akash.darknight@gmail.com>
2018-03-06 10:03:00 +0530

    Application file

--- DeepApp.py
+++ DeepApp.py
@@ -406,3 +406,8 @@ fname = "images/" + my_image
 image = np.array(ndimage.imread(fname, flatten=False))
 my_image = scipy.misc.imresize(image, size=(num_px, num_px)).reshape((num_px * num_px * 3, 1))
 my_predicted_image = predict(my_image, my_label_y, parameters)
+
+
+plt.imshow(image)
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classes[
+    int(np.squeeze(my_predicted_image)),].decode("utf-8") + "\" picture.")
